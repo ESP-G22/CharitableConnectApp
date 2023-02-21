c828f158998f5274d030f5af6a52b0c99dcd8605
Leonidas <mp2398@bath.ac.uk>
2023-02-21 20:41:48 +0000

    Added Explore, Profile and Feed fragments Added navigation bar Added nav host to activity_main.xml that will update based on navbar selection

--- app/src/main/java/dev/n0ne1eft/charitableconnect/MainActivity.java
+++ app/src/main/java/dev/n0ne1eft/charitableconnect/MainActivity.java
@@ -1,14 +1,28 @@
 package dev.n0ne1eft.charitableconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+public class MainActivity extends AppCompatActivity {
+    BottomNavigationView bottomNav;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Navigation Bar Code
+        bottomNav = findViewById(R.id.bottom_nav);
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.feedFragment, R.id.exploreFragment, R.id.profileFragment).build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_main_activity);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(bottomNav, navController);
     }
 }
