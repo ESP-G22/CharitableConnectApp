d30a4dc9f4adffb59ae5925f54066b79df848cac
joew5887 <joewill185@gmail.com>
2023-03-09 14:04:26 +0000

    Navigation for Registration of User.
    Fragments displayed using 'RegisterActivity'.
    Navigation through 'register_nav_graph'

--- app/src/main/java/dev/n0ne1eft/charitableconnect/RegisterActivity.java
+++ app/src/main/java/dev/n0ne1eft/charitableconnect/RegisterActivity.java
@@ -1,30 +1,22 @@
 package dev.n0ne1eft.charitableconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
 import android.os.Bundle;
 import android.view.View;
 
 import com.google.android.material.snackbar.Snackbar;
 
-import dev.n0ne1eft.charitableconnect.databinding.ActivityMainBinding;
-
 public class RegisterActivity extends AppCompatActivity {
-    private ActivityMainBinding binding;
+    private AppBarConfiguration appBarConfiguration;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.fragment_init_welcome);
-
-        //binding = ActivityMainBinding.inflate(getLayoutInflater());
-        //setContentView(binding.getRoot());
-
-        /*Navigation Bar Code
-        bottomNav = findViewById(R.id.bottom_nav);
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.feedFragment, R.id.exploreFragment, R.id.profileFragment).build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_main_activity);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(bottomNav, navController);*/
+        setContentView(R.layout.activity_register);
     }
 }
