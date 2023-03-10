3e14c9a314849677e905a236bb21210ae5350ec2
joew5887 <joewill185@gmail.com>
2023-03-10 11:24:18 +0000

    Updated endpoint urls

--- app/src/main/java/api/Util.java
+++ app/src/main/java/api/Util.java
@@ -1,5 +1,22 @@
 package api;
 
 public class Util {
-    public static final String ENDPOINT = "https://api.cc.n0ne1eft.dev/user/";
+    public static final String ENDPOINT = "https://api.cc.n0ne1eft.dev/";
+    public static final String ENDPOINT_USER = ENDPOINT + "user/";
+    public static final String ENDPOINT_USER_LIST = ENDPOINT_USER + "list";
+    public static final String ENDPOINT_LOGIN = ENDPOINT_USER + "login";
+    public static final String ENDPOINT_CHANGE_PASSWORD = ENDPOINT_USER + "passwordchange";
+    public static final String ENDPOINT_REGISTER = ENDPOINT_USER + "register";
+
+    public static final String ENDPOINT_EVENT = ENDPOINT + "events/";
+    public static final String ENDPOINT_EVENT_LIST = ENDPOINT_EVENT + "list";
+    public static final String ENDPOINT_EVENT_CREATE = ENDPOINT_EVENT + "create";
+
+    public static final String getUserEndpoint(int id) {
+        return ENDPOINT_USER + Integer.valueOf(id).toString();
+    }
+
+    public static final String getEventEndpoint(int id) {
+        return ENDPOINT_EVENT + Integer.valueOf(id).toString();
+    }
 }
